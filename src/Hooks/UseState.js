eff7b8b8f80aff5ce937fc0005399a571944d9bc
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-07 21:53:58 +0900

    Hook 1. useState()

---
+++ src/Hooks/UseState.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+export default function UseState() {
+  const [count, setCount] = useState(0);
+  const plusCount = () => {setCount((current)=>current+1);};
+  const minusCount = () => {setCount((current)=>current-1);};
+  return (
+    <div>
+      <span>{"1. useState() Hook => "}</span>
+      {count}
+      <button onClick={plusCount}>Up</button>
+      <button onClick={minusCount}>Down</button>
+    </div>
+  );
+}
